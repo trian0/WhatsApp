f2f24a06d982c8f65a0dfb49dab4cc2256c50146
Thales Rian <thalesrian0@gmail.com>
2022-05-29 11:28:09 -0300

    feat(MainActivity): adiciona itens de menu a toolbar principal

--- app/src/main/java/com/example/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/activity/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.whatsapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.whatsapp.R;
+import com.example.whatsapp.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,34 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
 
+        auth = ConfiguracaoFirebase.getFirebaseAuth();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_sair:
+                deslogarUsuario();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario(){
+        try {
+            auth.signOut();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
