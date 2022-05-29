643c15247d9550cea810418e48ea5de07b06ba88
Thales Rian <thalesrian0@gmail.com>
2022-05-29 01:04:33 -0300

    feat(CadastrarActivity): cria a activity o layout da tela de cadastro do usuário

---
+++ app/src/main/java/com/example/whatsapp/CadastroActivity.java
@@ -0,0 +1,15 @@
+package com.example.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+        getSupportActionBar().hide();
+    }
+}
