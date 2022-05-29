0ef9de1dc8d7e34500067cb961ee3258384ca37c
Thales Rian <thalesrian0@gmail.com>
2022-05-29 01:50:13 -0300

    chore(Activity): cria pastas para melhor organização do projeto

--- app/src/main/java/com/example/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/whatsapp/activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.whatsapp;
+package com.example.whatsapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.example.whatsapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
